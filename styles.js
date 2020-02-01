c39efc8a074ba65a18c0fd6e6e31e6f31f4e517b
Nick Basile <nicholas.basile@outlook.com>
2020-01-31 21:52:10 -0600

    Removed boilerplate in App.js and added custom fonts

---
+++ styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  pageTitle: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 18,
+    letterSpacing: -0.01,
+    color: 'rgb(10, 180, 99)',
+  },
+});
+
+export default styles;
